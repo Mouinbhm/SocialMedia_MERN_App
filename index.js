427cc2460cd0fd150ef6a5375a5b8414f55e03db
Mouinbhm <mouinbelhaj@icloud.com>
2024-04-29 11:14:49 +0100

    Connect the app with mongodb using mongoose

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("Welcome to the home page!");
+});
+
+mongoose.connect("mongodb://localhost:27017/SocialMediaApp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+  console.log("Connected to MongoDB on " + db.host + "/" + db.name);
 });
 
 app.listen(process.env.PORT, () => {
