6b753b1d4943d85bdf12984fe10524b8e75a44e6
Mouinbhm <mouinbelhaj@icloud.com>
2024-05-02 20:57:44 +0100

    Create SignIn function

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,5 +1,6 @@
 const user = require("../Models/user.model");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res) => {
   const data = {
@@ -19,4 +20,34 @@ exports.signup = (req, res) => {
     .catch((err) => {
       res.status(400).json({ message: "Server error" });
     });
+};
+
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await user.findOne({ email: email });
+    if (!existingUser) {
+      return res.status(400).json({ message: "Email invalide" });
+    }
+    bcrypt.compare(password, existingUser.password, (err, isMatch) => {
+      if (err) {
+        return res.status(500).json({ message: "Erreur du serveur" });
+      }
+      if (!isMatch) {
+        return res.status(400).json({ message: "Mot de passe incorrect" });
+      }
+      const token = jwt.sign(
+        { data: { id: existingUser.id, role: existingUser.role } },
+        process.env.CLE,
+        { expiresIn: "1h" }
+      );
+      return res.status(200).json({
+        message: "Connexion réussie",
+        token: token,
+        user: existingUser,
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erreur du serveur" });
+  }
 };
--- Routes/user.router.js
+++ Routes/user.router.js
@@ -10,6 +10,7 @@ const userController = require("../Controllers/user.controller");
 // Setting up a POST request handler for the /user/signup route
 // This will delegate the request handling to the signup method of the userController module
 route.post("/signup", userController.signup);
+route.post("/signin", userController.signin);
 
 // Exporting the configured router to be used in other parts of the application
 module.exports = route;
