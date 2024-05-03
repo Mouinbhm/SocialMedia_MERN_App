6c0086bc797930760436629354c594f85f878212
Mouinbhm <mouinbelhaj@icloud.com>
2024-05-03 11:33:46 +0100

    Retrive connected user data from localStorage

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [connectedUser, setConnectedUser] = useState(null); // Initialiser à null pour éviter les erreurs de rendu
+
+  const getConnectedUserData = () => {
+    const userData = localStorage.getItem("user_data");
+    if (userData) {
+      setConnectedUser(JSON.parse(userData));
+    }
+  };
+
+  useEffect(() => {
+    getConnectedUserData();
+  }, []);
+
   return (
     <>
-      <h1>Hello user logged in</h1>
+      {connectedUser ? (
+        <h1>Hello {connectedUser.firstname}</h1>
+      ) : (
+        <h1>Hello Guest</h1>
+      )}
     </>
   );
 };
